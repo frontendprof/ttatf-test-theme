657afe457fb7f6cf5a308a9590f14dc6360da93c
Abdumalik <abdumaliksharipov@gmail.com>
2024-02-11 09:10:54 +0500

    feat: add custom query pagination

--- page-past-events.php
+++ page-past-events.php
@@ -5,16 +5,35 @@
     <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ttatf-4.jpg'); ?>)"></div>
           <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title">All events </h1>
+            <h1 class="page-banner__title">Past events </h1>
             <div class="page-banner__intro">
-              <p>See what is happening in our campus.</p>
+              <p>Allaqachon bo'lib o'tgan tadbirlar </p>
             </div>
           </div>
     </div>
 
     <div class="container container--narrow page-section"> 
-      <?php while(have_posts()) {
-        the_post(); ?>
+        
+      <?php 
+       $today=date('Ymd');
+       $pastEvents = new WP_Query(array(
+        'paged'=>get_query_var('paged',1),
+         'post_type'=>'event',
+         'meta_key'=>'event_date',
+         'orderby'=>'meta_value_num',
+         'order'=>'ASC',
+         'meta_query'=>array(
+           array(
+             'key'=>'event_date',
+             'compare'=>'<',
+             'value'=>$today,
+             'type'=>'numeric'
+           )
+         )
+       ));
+
+      while($pastEvents->have_posts()) {
+        $pastEvents->the_post(); ?>
         <div class="event-summary">
                  <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
                   <span class="event-summary__month"><?php $eventDate=new DateTime(get_field('event_date')); echo $eventDate->format('M'); ?></span>
@@ -26,12 +45,10 @@
                 </div>
               </div>
        <?php }
-        echo paginate_links();
+        echo paginate_links(array(
+            'total'=>$pastEvents->max_num_pages
+        ));
       ?>
-
-      <hr class="section-break">
-
-      <p>Looking for a recap of past events?  <a href="<?php echo site_url('/past-events'); ?>">Check out our past events archive.</a></p>
     </div>
 
 
