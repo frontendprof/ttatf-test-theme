4a6fab2e4346cc41b0d176092707a5cebc7e14fe
Abdumalik <abdumaliksharipov@gmail.com>
2024-02-11 15:02:16 +0500

    feat: add page banner dynamic background image

--- single-professor.php
+++ single-professor.php
@@ -4,11 +4,13 @@
         the_post(); ?>
         
     <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ttatf-5.jpg'); ?>)"></div>
+      <div class="page-banner__bg-image" 
+      style="background-image: url(<?php $pageBannerImage=get_field('page_banner_background_image');
+      echo $pageBannerImage['sizes']['pageBanner'] ?>)"></div>
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php the_title(); ?> </h1>
         <div class="page-banner__intro">
-          <p>Here goes subtitle, remeber.</p>
+          <p><?php the_field('page_banner_subtitle'); ?></p>
         </div>
       </div>
     </div>
